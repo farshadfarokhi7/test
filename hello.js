775406d9c5aa55dc6d69599a40a790b0e127539f
FarshadFarokhi <farshadfarokhi.cse@gmail.com>
2020-08-31 12:22:24 +0430

    initial commit

---
+++ hello.js
@@ -0,0 +1,16 @@
+console.log('Hello World!!!')
+
+const add = (a,b, callback) => {
+    setTimeout( () => {
+        const sum = a+b
+        callback(sum)
+    }
+    ,2000)
+}
+
+
+add(1,4, (sum) => {
+    console.log(sum)
+})
+
+
